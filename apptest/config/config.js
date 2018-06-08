1bf185c25cecb2390af10fa5c425adba7ebd0a84
HarounaSY <HarounaSY@192.168.1.104>
2018-06-08 12:52:51 +0000

    routeur du module grade et connexion à mongodb depuis mlab

--- apptest/config/config.js
+++ apptest/config/config.js
@@ -1,17 +1,25 @@
 
 // Database name
-const dbname = 'pptgendarmeriesn';
+const dbname = 'gdmgrh';
+const dbuser = 'gendarmerie';
+const dbpassword ='P@ssw0rdgendarmerie';
 
 module.exports = {
 	// Server Port number
 	port: process.env.PORT || 3000,
 
+
 	// Server IP address
-	ip: process.env.IP || 'localhost',
+	//ip: process.env.IP || 'localhost',
+	ip: process.env.IP || 'https://mlab.com/login/',
+
 
 	// Mongoose config
 	mongo: {
-		uri: `mongodb://localhost:27017/${dbname}`,
+		//uri: `mongodb://localhost:27017/${dbname}`,
+		uri: 'mongodb://'+dbuser+':'+dbpassword+'@ds147190.mlab.com:47190/database_gdmgrh',
+
+
 		option: {
 			autoIndex: false, // Don't build indexes
 			reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
