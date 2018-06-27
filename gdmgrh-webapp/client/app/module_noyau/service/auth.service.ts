1718802cb24ce98ed95f3ec5004e9f32e04894a3
cmpseve <cezechiel81@gmail.com>
2018-06-27 12:16:48 +0000

    feature(AuthService: fontend): Authentification service

---
+++ gdmgrh-webapp/client/app/module_noyau/service/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { localStorageName } from '../../app.constants';
+
+@Injectable()
+export class AuthService {
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    /**
+     * Save user in local starage
+     */
+    save (user) {
+        localStorage.setItem(localStorageName, JSON.stringify(user));
+    }
+
+    /**
+     * check user is connected
+     */
+    isConnected() {
+        return localStorage.getItem(localStorageName) !== null;
+    }
+
+    /**
+     * get user connected
+     */
+    getUser() {
+        if (this.isConnected) {
+            return JSON.parse(localStorage.getItem(localStorageName));
+        }
+        return null;
+    }
+
+    /**
+     * logout User
+     */
+    logout() {
+        localStorage.clear();
+    }
+
+    /**
+     * Sign-in User
+     */
+    signin(user) {
+
+    }
+}
