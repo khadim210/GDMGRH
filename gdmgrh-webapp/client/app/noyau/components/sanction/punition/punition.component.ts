bda8df1513c8629070715ca464079d4c1b096c06
hermanang <tobnang1234@gmail.com>
2018-07-10 03:13:23 +0200

    mise a jour sanction

--- gdmgrh-webapp/client/app/noyau/components/sanction/punition/punition.component.ts
+++ gdmgrh-webapp/client/app/noyau/components/sanction/punition/punition.component.ts
@@ -6,48 +6,59 @@ import { interval } from 'rxjs/observable/interval';
 
 @Component({
     selector: 'selector-punition',
-    template: require('./punition.component.html')
+    template: require('./punition.component.html'),
+    styles: [`
+      @keyframes masuperanimation {
+        0% {
+            transform: translateX(150px);
+        }
+        -50% {
+            transform: translateX(150px);
+        }
+        -100% {
+            transform: translateX(150px) rotate(15deg);
+        }
+      }
+      .w3-display-topright {
+        position: absolute;
+        right: 0;
+        top: 0
+      }
+      .w3-padding {
+        padding: 8px 16px!important
+      }
+      .alert {
+        animation: masuperanimation 2s;
+      }
+
+    `]
 })
 export class PunitionComponent implements OnInit {
 
-    datas: PunitionModel[];
-    recompenses: PunitionModel[];
+    punition_data: any[];
 
-    libelles: any;
     punition_edited: PunitionModel;
     type_punition_edited: String;
     type_officier_edited: String;
     nom_punition_edited: String;
 
     code_deleted: any;
-    succes_action = false;
-
-    filter: String;
 
     constructor(
       private recompenseService: SanctionService
     ) {
-        this.datas = [];
-        this.recompenses = [];
-        this.libelles = [];
+        this.punition_data = [];
     }
 
 
   ngOnInit() {
 
     this.recompenseService.getPunitions()
-      .subscribe(data => this.datas = data);
-
-      // for (let _i = 0; _i < data.length; _i++) {
-          // for (let _j = 0; _j < data[_i].intitules.length; _j++) {
-            // tslint:disable-next-line:max-line-length
-            // this.datas.push({_id: data[_i].intitules[_j]['_id'], nom_recompense: data[_i].intitules[_j]['nom_recompense'], type_recompense: data[_i].type_recompense});
-          // }
-        // }
+      .subscribe(data => this.punition_data = data);
   }
   btnEdit(item: PunitionModel): void {
     this.punition_edited = item;
-    // this.name_edited = this.recompense_edited.nom_recompense;
+
     this.type_punition_edited = item.type_punition;
     this.type_officier_edited = item.type_officier;
     this.nom_punition_edited = item.nom_punition;
@@ -57,24 +68,18 @@ export class PunitionComponent implements OnInit {
     this.code_deleted = item;
   }
 
-  delete(): void {
-    // tslint:disable-next-line:prefer-const
-    let index = this.datas.indexOf(this.code_deleted);
-
-    if (index >= 0) {
-      this.datas.splice(index, 1);
+  deletePunition(): void {
       this.recompenseService.deletePunition(this.code_deleted._id)
       .subscribe(
         data => {
-          this.alert_action();
+          this.punition_data = data;
         }, error => {
           console.log(error);
         }
       );
-    }
   }
 
-  editerRecompense(type_p, type_o, nom_p): void {
+  upgradePunition(type_p, type_o, nom_p): void {
 
    this.punition_edited.nom_punition = nom_p;
    this.punition_edited.type_officier = type_o;
@@ -83,12 +88,12 @@ export class PunitionComponent implements OnInit {
     this.recompenseService.editPunition(this.punition_edited)
     .subscribe(
       data => {
-        this.alert_action();
+        this.punition_data = data;
       }
     );
   }
 
-  recordPunition(item: NgForm): void {
+  addPunition(item: NgForm): void {
     const tamponPunition = {
       type_punition: item.value.type_punition,
       type_officier: item.value.type_officier,
@@ -97,17 +102,9 @@ export class PunitionComponent implements OnInit {
     this.recompenseService.addPunition(tamponPunition)
       .subscribe(
         data => {
-          // tslint:disable-next-line:max-line-length
-          this.datas.push({_id: data[' _id '], type_punition: data[' type_punition '], type_officier: data[' type_officier '], nom_punition: data[' nom_punition ']});
-          this.alert_action();
+          this.punition_data = data;
         }
       );
-  }
-
-  alert_action() {
-    this.succes_action = true;
-    interval(6500)
-      .subscribe(res => this.succes_action = false);
   }
 
 }
--- gdmgrh-webapp/client/app/noyau/components/sanction/recompense/recompense.component.ts
+++ gdmgrh-webapp/client/app/noyau/components/sanction/recompense/recompense.component.ts
@@ -6,39 +6,51 @@ import { interval } from 'rxjs/observable/interval';
 
 @Component({
     selector: 'selector-recompense',
-    template: require('./recompense.component.html')
+    template: require('./recompense.component.html'),
+    styles: [`
+      @keyframes masuperanimation {
+        0% {
+            transform: translateX(150px);
+        }
+        -50% {
+            transform: translateX(150px);
+        }
+        -100% {
+            transform: translateX(150px) rotate(15deg);
+        }
+      }
+      .w3-display-topright {
+        position: absolute;
+        right: 0;
+        top: 0
+      }
+      .w3-padding {
+        padding: 8px 16px!important
+      }
+      .alert {
+        animation: masuperanimation 2s;
+      }
+
+    `]
 })
 export class RecompenseComponent implements OnInit {
 
-    datas: RecompenseModel[];
-    recompenses: RecompenseModel[];
-
-    libelles: any;
+    recompense_data: any[];
     recompense_edited: RecompenseModel;
     nom_recompense_edited: String;
     type_recompense_edited: String;
-    code_deleted: any;
-    succes_action = false;
+    code_to_delete: any;
 
   constructor(
       private sanctionService: SanctionService
     ) {
-        this.datas = [];
-        this.recompenses = [];
-        this.libelles = [];
+        this.recompense_data = [];
     }
 
 
   ngOnInit() {
     this.sanctionService.getRecompenses()
-      .subscribe(data => this.datas = data);
-
-      // for (let _i = 0; _i < data.length; _i++) {
-          // for (let _j = 0; _j < data[_i].intitules.length; _j++) {
-            // tslint:disable-next-line:max-line-length
-            // this.datas.push({_id: data[_i].intitules[_j]['_id'], nom_recompense: data[_i].intitules[_j]['nom_recompense'], type_recompense: data[_i].type_recompense});
-          // }
-        // }
+      .subscribe(data => this.recompense_data = data);
   }
   btnEdit(item: RecompenseModel): void {
     this.recompense_edited = item;
@@ -47,39 +59,33 @@ export class RecompenseComponent implements OnInit {
   }
 
   btnDelete(item: any): void {
-    this.code_deleted = item;
+    this.code_to_delete = item;
   }
 
-  delete(): void {
-    // tslint:disable-next-line:prefer-const
-    let index = this.datas.indexOf(this.code_deleted);
-
-    if (index >= 0) {
-      this.datas.splice(index, 1);
-      this.sanctionService.deleteRecompense(this.code_deleted._id)
-      .subscribe(
-        data => {
-          this.alert_action();
-        }, error => {
-          console.log(error);
-        }
-      );
-    }
+  deleteRecompense(): void {
+    this.sanctionService.deleteRecompense(this.code_to_delete._id)
+    .subscribe(
+      data => {
+        this.recompense_data = data;
+      }, error => {
+        console.log(error);
+      }
+    );
   }
 
-  editerRecompense(type_rec, nom_rec): void {
+  upgradeRecompense(type_rec, nom_rec): void {
     this.recompense_edited.type_recompense = type_rec;
     this.recompense_edited.nom_recompense = nom_rec;
 
     this.sanctionService.editRecompense(this.recompense_edited)
     .subscribe(
       data => {
-        this.alert_action();
+        this.recompense_data = data;
       }
     );
   }
 
-  recordRecompense(item: NgForm): void {
+  addRecompense(item: NgForm): void {
     const tamponRecompense = {
       type_recompense: item.value.type_recompense,
       nom_recompense: item.value.nom_recompense
@@ -88,16 +94,9 @@ export class RecompenseComponent implements OnInit {
     this.sanctionService.addRecompense((tamponRecompense) as RecompenseModel)
       .subscribe(
         data => {
-          this.datas.push({_id: data[' _id '], nom_recompense: data[' nom_recompense '], type_recompense: data[' type_recompense ']});
-          this.alert_action();
+          this.recompense_data = data;
         }
       );
-  }
-
-  alert_action() {
-    this.succes_action = true;
-    interval(6500)
-      .subscribe(res => this.succes_action = false);
   }
 
 }
